c6d5d4ac25d8fb265840fdbdf6250e6d213f8a0e
Olga Sin <nyak.shnyak@gmail.com>
2021-10-29 23:22:59 +0300

    магазин сигарет, 1 версия

---
+++ src/Cigarettes.java
@@ -0,0 +1,7 @@
+public class Cigarettes {
+    public static void main(String[] args) {
+        String welcome = "Добро пожаловать в наш магазин!";
+        String offer = "Попробуйте наши новые солевые жижки со вкусом арбуза и тархуна!";
+        System.out.println(welcome + " " + offer);
+    }
+}
