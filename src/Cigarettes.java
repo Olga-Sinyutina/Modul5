cb122788ad56de3d3ca86860f5f6878de326e9a7
Olga Sin <nyak.shnyak@gmail.com>
2021-10-29 23:58:59 +0300

    check_age

--- src/Cigarettes.java
+++ src/Cigarettes.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Cigarettes {
     public static void main(String[] args) {
         String welcome = "Добро пожаловать в наш магазин!";
-        String offer = "Попробуйте наши новые солевые жижки со вкусом арбуза и тархуна!";
-        System.out.println(welcome + " " + offer);
+        System.out.println(welcome);
+        ageCheck();
     }
+
+    public static void ageCheck() {
+        String offer = "Попробуйте наши новые солевые жижки со вкусом арбуза и тархуна!";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Подтвердите, что вам есть 18 лет.");
+        String confirmation = scanner.nextLine();
+        if (confirmation.equals("yes")) {
+            System.out.println(offer);
+        } else System.out.println("Простите, мы не можем продавать никотиносодержащие товары лицам младше 18 лет:(");
+    }
+
 }
